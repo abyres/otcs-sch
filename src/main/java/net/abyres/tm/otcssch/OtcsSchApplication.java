126c1717e3ce0d79148ecea4ba7d5b4bcad2cefe
Onn Khairuddin bin Rahmat <onn.khairuddin@gmail.com>
2018-03-11 19:01:32 +0800

    Initial import Scheduling Management.

---
+++ src/main/java/net/abyres/tm/otcssch/OtcsSchApplication.java
@@ -0,0 +1,12 @@
+package net.abyres.tm.otcssch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtcsSchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtcsSchApplication.class, args);
+	}
+}
